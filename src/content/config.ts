4b4fefdae543fe6dacfe1b06b22fd13474c4e713
Matt Batman <batman.matt@gmail.com>
2024-09-11 11:58:53 -0400

    Start displaying content from markdown

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define your collection(s)
+const videoCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    channel: z.string(),
+    tags: z.array(z.string()),
+    added: z.date(),
+    platform: z.string(),
+    "video-tags": z.array(z.string()),
+  }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  videos: videoCollection,
+};
---
+++ src/content/videos/1-pineapple-habanero-hot-sauce.md
@@ -0,0 +1,14 @@
+---
+id: 1
+title: Pineapple-Habanero Hot Sauce - Chili Pepper Madness
+channel: Chili Pepper Madness
+tags:
+  - youtube
+  - hot-sauce
+added: 2024-09-09
+platform: YouTube
+video-tags:
+  - hot-sauce
+  - recipes
+---
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/xJ2z2JXGoIE?si=xgH00nkBognMidw-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
