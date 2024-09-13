f3b31f10dad802a2f87e55e739bc88a365d2bcb5
Matt Batman <batman.matt@gmail.com>
2024-09-12 22:38:12 -0400

    Add site deploy as action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://mattbatman.github.io',
+	base: 'not-a-rabbit-hole',
+});
