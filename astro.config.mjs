dde0821b9021802e4ff69b12ca1ce83e80bb2c94
Matt Batman <batman.matt@gmail.com>
2024-09-13 09:28:21 -0400

    Custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mattbatman.github.io",
-  base: "not-a-rabbit-hole",
+  site: "https://notarabbithole.fyi",
 });
